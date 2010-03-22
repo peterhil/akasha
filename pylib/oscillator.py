cf6de750e53c4deee9d40f1c25b14c422506955c
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-03-23 01:50:38 +0200

    Made SampleRate a descriptor class and moved trial versions into classes.py

--- pylib/oscillator.py
+++ pylib/oscillator.py
@@ -4,25 +4,61 @@
 from fractions import Fraction
 from cmath import rect, polar, phase, pi
 import numpy as np
+from recipes import prop
 
-# def root(x):
-#     cmath.polar(1, cmath.pi*x/period)
+class SampleRate(object):
+    """
+    A descriptor object for sample rate.
+    TODO: also handle sending resampling calls to objects?
+    """
+    default_rate = 44100
+    
+    def __init__(self, value=default_rate):
+        self.rate = value
+    
+    def __get__(self, obj, objtype):
+        print "Self: %s, Obj: %s, Object type: %s" % (self.rate, obj, objtype)
+        return self.rate or __set__(default_rate)
+    
+    def __set__(self, obj, rate):
+        print "set"
+        self.rate = rate
 
 class Osc:
     """Oscillator class"""
     roots = dict()
+    tuning = SampleRate()
     
-    def __init__(self, ratio):
-        self.order, self.period = ratio.numerator, ratio.denominator
+    def __init__(self, *args):
+        # Set ratio and limit between 0/1 and 1/1
+        self.ratio = Fraction(*args) % Fraction(1)
         
         root = lambda x: rect(1, 2 * pi * Fraction(x, self.period))
-        if not Osc.roots.has_key(ratio):
-            Osc.roots[ratio] = np.array(map(root, range(0, self.period)), dtype=complex)
-        self.roots = Osc.roots[ratio]
+        if not Osc.roots.has_key(self.period):
+            Osc.roots[self.period] = np.array(map(root, range(0, self.period)), dtype=complex)
+        self.roots = Osc.roots[self.period]
     
+    @property
+    def ratio(self):
+        return self.ratio
+        
+    @property
+    def period(self):
+        return self.ratio.denominator
+        
+    @property
+    def order(self):
+        return self.ratio.numerator % self.period
+        
+    def __eq__(self, other):
+        return self.ratio == other.ratio
+        
+    def __repr__(self):
+        return "Osc(%s)" % self.ratio.__repr__()
+        
 if __name__ == '__main__':
     o = Osc(Fraction(1, 8))
     print o.roots
-    print map(lambda x: phase(x) / (2 * pi) * 360, o.roots)
+    print map(lambda x: (abs(x), (phase(x) / (2 * pi) * 360)), o.roots)
     print Osc.roots.keys()
     # print Osc(Fraction('1/2')).roots
--- pylib/oscillatortest.py
+++ pylib/oscillatortest.py
@@ -8,17 +8,31 @@ from oscillator import *
 from fractions import Fraction
 import unittest
 
+class TuningTest(unittest.TestCase):
+    """Tests for asserting that Osc has access to sample rate."""
+    def testTuning(self):
+        self.failUnlessRaises(AttributeError, Osc.tuning)
+        Osc.tuning = 44100
+        self.assertEqual(44100, Osc.tuning)
+    
 class OscInitTest(unittest.TestCase):
     def setUp(self):
-        self.o = Osc(Fraction(1,8))
-    
+        self.o8 = Osc(Fraction(1,8))
+        self.p8 = Osc(Fraction(9,8))
+        self.m8 = Osc(Fraction(-1,8))
+        
     def testInit(self):
         """Test initialization"""
-        assert(isinstance(self.o, Osc))
-    
+        assert(isinstance(self.o8, Osc))
+        
+    def testModularWrap(self):
+        self.assertEqual(self.o8, self.p8)
+        self.assertEqual(7, self.m8.order)
+        
     def testSize(self):
         """Test size of roots"""
-        self.assertEqual(8, self.o.roots.size)
+        self.assertEqual(8, self.o8.roots.size)
+    
 
 if __name__ == "__main__":
     unittest.main()
