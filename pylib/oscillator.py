3bb1cb2c9e53ddb814b6d6fa5fbb05e49dd02f5e
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-03-28 20:32:36 +0300

    Oscillator now works with three different generating functions and it has tests. Ordering and sampling needs to be done yet.

--- pylib/oscillator.py
+++ pylib/oscillator.py
@@ -2,63 +2,115 @@
 # -*- coding: utf-8 -*-
 
 from fractions import Fraction
-from cmath import rect, polar, phase, pi
+from cmath import rect, polar, phase, pi, exp
 import numpy as np
-from recipes import prop
+import types
 
-class SampleRate(object):
-    """
-    A descriptor object for sample rate.
-    TODO: also handle sending resampling calls to objects?
-    """
-    default_rate = 44100
-    
-    def __init__(self, value=default_rate):
-        self.rate = value
-    
-    def __get__(self, obj, objtype):
-        print "Self: %s, Obj: %s, Object type: %s" % (self.rate, obj, objtype)
-        return self.rate or __set__(default_rate)
-    
-    def __set__(self, obj, rate):
-        print "set"
-        self.rate = rate
+def to_phasor(x):
+    return (abs(x), (phase(x) / (2 * pi) * 360))
+
+def limit_ratio(f):
+    if Osc.prevent_aliasing and abs(f) > Fraction(1,2):
+        return Fraction(0, 1)
+    if Osc.prevent_aliasing and not Osc.negative_frequencies and f < 0:
+        return Fraction(0, 1)
+    # wrap roots: 9/8 == 1/8 in Osc! This also helps with numeric accuracy.
+    n = f.numerator % f.denominator
+    return Fraction(n, f.denominator)
+
+class Sampler:
+    rate = 44100
 
 class Osc:
     """Oscillator class"""
+    
+    # Settings
+    prevent_aliasing = True
+    negative_frequencies = False
+    
+    # Root cache is a dictionary
     roots = dict()
-    tuning = SampleRate()
     
     def __init__(self, *args):
         # Set ratio and limit between 0/1 and 1/1
-        self.ratio = Fraction(*args) % Fraction(1)
+        self.ratio = limit_ratio(Fraction(*args))
         
-        root = lambda x: rect(1, 2 * pi * Fraction(x, self.period))
         if not Osc.roots.has_key(self.period):
-            Osc.roots[self.period] = np.array(map(root, range(0, self.period)), dtype=complex)
+            Osc.roots[self.period] = self.table_roots() # self.gen_roots(self.nth_root)
         self.roots = Osc.roots[self.period]
+        self.roots = self.ord_roots()
+    
+    @classmethod
+    def freq(self, freq):
+        """Oscillator can be initialized using a frequency, which can be float."""
+        ratio = Fraction.from_float(float(freq)/Sampler.rate).limit_denominator(Sampler.rate)
+        return Osc(ratio)
+    
+    def _order_index(self, ratio):
+        order, period = ratio.numerator, ratio.denominator
+        return np.array(range(period)) * order % period
+        
+    def ord_roots(self):
+        if self.order == 1:
+            return self.roots
+        else:
+            return self.roots[self._order_index(self.ratio)]
+    
+    def normalize_fraction(self, n):
+        n %= self.period
+        return Fraction(n, self.period)
+    
+    def nth_root(self, n):
+        w = 2 * pi
+        return rect(1, w * self.normalize_fraction(n))
+    
+    def func_root(self, n):
+        wi = 2 * pi * 1j
+        return exp(wi * self.normalize_fraction(n))
+    
+    def gen_roots(self, func):
+        return np.array(
+            map( func, range(0, self.period) ),
+            dtype=complex
+        )
+    
+    def table_roots(self):
+        if not Osc.roots.has_key(Sampler.rate):
+            Osc.roots[Sampler.rate] = self.gen_table_roots()
+        return Osc.roots[Sampler.rate][0:self.period] ** (Sampler.rate / float(self.period))
+    
+    def gen_table_roots(self):
+        w = 2 * pi
+        nth_root = lambda n: rect(1, w * Fraction(n, Sampler.rate))
+        return np.array(map(nth_root, range(0, Sampler.rate)))
+    
+    @property
+    def frequency(self):
+        return float(self.ratio * Sampler.rate)
     
     @property
     def ratio(self):
         return self.ratio
-        
+    
     @property
     def period(self):
         return self.ratio.denominator
-        
+    
     @property
     def order(self):
-        return self.ratio.numerator % self.period
-        
+        return self.ratio.numerator
+    
     def __eq__(self, other):
         return self.ratio == other.ratio
-        
+    
     def __repr__(self):
-        return "Osc(%s)" % self.ratio.__repr__()
-        
+        return "Osc(%s, %s)" % (self.order, self.period)
+    
+    def to_phasors(self):
+        return np.array(map(to_phasor, self.roots))
+    
 if __name__ == '__main__':
     o = Osc(Fraction(1, 8))
     print o.roots
-    print map(lambda x: (abs(x), (phase(x) / (2 * pi) * 360)), o.roots)
+    print o.to_phasors()
     print Osc.roots.keys()
-    # print Osc(Fraction('1/2')).roots
--- pylib/oscillatortest.py
+++ pylib/oscillatortest.py
@@ -8,31 +8,113 @@ from oscillator import *
 from fractions import Fraction
 import unittest
 
-class TuningTest(unittest.TestCase):
-    """Tests for asserting that Osc has access to sample rate."""
-    def testTuning(self):
-        self.failUnlessRaises(AttributeError, Osc.tuning)
-        Osc.tuning = 44100
-        self.assertEqual(44100, Osc.tuning)
-    
 class OscInitTest(unittest.TestCase):
-    def setUp(self):
-        self.o8 = Osc(Fraction(1,8))
-        self.p8 = Osc(Fraction(9,8))
-        self.m8 = Osc(Fraction(-1,8))
-        
     def testInit(self):
         """Test initialization"""
-        assert(isinstance(self.o8, Osc))
-        
-    def testModularWrap(self):
-        self.assertEqual(self.o8, self.p8)
-        self.assertEqual(7, self.m8.order)
-        
+        o = Osc(1,8)
+        assert(isinstance(o, Osc))
+        self.assertEqual(o.order, 1)
+        self.assertEqual(o.period, 8)
+        self.assertEqual(o.ratio, Fraction(1,8))
+    
     def testSize(self):
         """Test size of roots"""
-        self.assertEqual(8, self.o8.roots.size)
+        self.assertEqual(8, Osc(1,8).roots.size)
     
+    def testInitWithAliasing(self):
+        Osc.prevent_aliasing = False
+        Osc.negative_frequencies = True
+        self.assertEqual(Osc(1,8), Osc(9,8))
+        self.assertEqual(Osc(7,8), Osc(-1,8))
+
+
+class OscFrequencyTest(unittest.TestCase):
+    """Test frequencies"""
+    def testFreqInit(self):
+        """It should intialize using a frequency"""
+        o = Osc.freq(440)
+        assert(isinstance(o, Osc))
+        assert(isinstance(o.frequency, float))
+    
+    def testFrequency440(self):
+        """It should return correct frequency for 440 Hz."""
+        o = Osc.freq(440)
+        self.assertEqual(o.frequency, 440)
+    
+    def testFloatFrequency(self):
+        """It should return close frequency from float."""
+        o = Osc.freq(440.899)
+        self.assertAlmostEqual(o.frequency, 440.899, places=4)
+
+
+class OscRootsTest(unittest.TestCase):
+    """Test root generating functions."""
+    from cmath import exp
+    
+    def testRootFuncSanity(self):
+        """It should give sane values."""
+        wi = 2 * pi * 1j
+        a = Osc(1,8).roots
+        b = np.array([
+            +1+0j, exp(wi*1/8),
+            +0+1j, exp(wi*3/8),
+            -1+0j, exp(wi*5/8),
+            -0-1j, exp(wi*7/8),
+        ])
+        assert np.allclose(a.real, b.real, atol=2e-15), \
+            "real %s is not close to\n\t\t%s" % (a, b)
+        assert np.allclose(a.imag, b.imag, atol=2e-15), \
+            "imag %s is not close to\n\t\t%s" % (a, b)
+    
+    def testPhasors(self):
+        """It should be accurate.
+        Uses angles to make testing easier.
+        """
+        for period in (5,7,8,23):   # + tuple( random.choice(range(1, 44100)) )
+            o = Osc(Fraction(1, period))
+            fractional_angle = lambda n: float(Fraction(n, period) % 1) * 360
+            angles = np.array( map( fractional_angle, range(0,period) ) )
+            angles = 180 - ( (180 - angles) % 360) # wrap 'em to -180..180!
+            a = o.to_phasors()
+            b = np.array( zip( [1] * period,  angles ) )
+            assert np.allclose(a, b), \
+                "%s is not close to\n\t\t%s" % (a, b)
+
+
+class OscAliasingTest(unittest.TestCase):
+    """Test (preventing the) aliasing of frequencies: f < 0, f > sample rate"""
+    silence = Osc(0, 1)
+    
+    def testFrequenciesOverNyquistRate(self):
+        """Is should (not) produce silence if ratio > 1/2"""
+        Osc.prevent_aliasing = True
+        self.assertEqual(Osc(9, 14), self.silence)
+        
+        Osc.prevent_aliasing = False
+        self.assertEqual(Osc(9,14).ratio, Fraction(9, 14))
+    
+    def testFrequenciesOverSamplingRate(self):
+        Osc.prevent_aliasing = True
+        self.assertEqual(Osc(9, 7), self.silence)
+        
+        Osc.prevent_aliasing = False    # but still wraps when ratio > 1
+        self.assertEqual(Osc(9, 7).ratio, Fraction(2, 7))
+    
+    def testNegativeFrequencies(self):
+        """It should handle negative preferences according to preferences."""
+        Osc.prevent_aliasing = True
+        Osc.negative_frequencies = False
+        self.assertEqual(Osc(-1, 7), self.silence)
+        
+        Osc.prevent_aliasing = False
+        Osc.negative_frequencies = True
+        self.assertEqual(Osc(-1, 7), Osc(6, 7))
+        
+        Osc.prevent_aliasing = True
+        Osc.negative_frequencies = True
+        ratio = Fraction(-1, 7)
+        o = Osc(ratio)
+        self.assertEqual(o.frequency, float((ratio * Sampler.rate) % Sampler.rate))
 
 if __name__ == "__main__":
     unittest.main()
