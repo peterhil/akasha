48ee3f8061e10e4075c1a672cadf552980c451da
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-05-05 22:47:12 +0300

    Added self.freq.

--- pylib/harmonics.py
+++ pylib/harmonics.py
@@ -30,8 +30,8 @@ class Harmonic(Generator):
         oscs = np.ma.masked_array(oscs, np.equal(oscs, Osc(0, 1)), None).compressed()
         frames = np.zeros(len(iter), dtype=complex)
         for o in oscs:
-            # e = Exponential(0, amp=float(freq)/o.frequency*float(freq)) # square waves
-            # e = Exponential(0, amp=float(freq)**2/o.frequency**2*float(freq)) # triangle waves
+            # e = Exponential(0, amp=float(self.freq)/o.frequency*float(self.freq)) # square waves
+            # e = Exponential(0, amp=float(self.freq)**2/o.frequency**2*float(self.freq)) # triangle waves
             e = Exponential(-o.frequency/100.0) # sine waves
             frames += o[iter] * e[iter]
         return frames / max( abs(max(frames)), len(oscs), 1.0 )
