baf3509d5802cad3e175ae185554887784257687
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-05-12 18:30:00 +0300

    Simplified sound class to use just array instead of defaultdict for sounds.

--- pylib/sound.py
+++ pylib/sound.py
@@ -8,33 +8,30 @@ from scikits.audiolab import play, wavwrite
 
 from envelope import Exponential
 from oscillator import Osc
+from generators import Generator
 
 # np.set_printoptions(precision=4, suppress=True)
 
-class Sound(defaultdict):
+class Sound(Generator):
     """Sound groups."""
 
-    def __init__ (self):
-        self.sounds = defaultdict.__init__(self, object)
+    def __init__ (self, *args):
+        self.sounds = list(args)
 
     def sample(self, iter):
         '''Pass parameters to all sound objects and update states.'''
         sound = np.zeros(len(iter), dtype=complex)
         for sndobj in self.sounds:
-            sound += sndobj.sample(iter)
-        return sound / max( abs(max(sound)), len(sound), 1.0 )
+            sound += sndobj[iter]
+        return sound / max( len(self.sounds), 1.0 )
 
-    def add(self, sndobj, offset=0):
-        '''Add a new sndobj to self.'''
-        self.sounds[sndobj] = sndobj
+    # Use s.sounds.append(sndobj)
+    # s.sounds.index(sndobj)
 
-    def stat(self):
-        '''Return a tuple containing the state of each sound object.'''
-        return tuple(self.sounds.values())
-
-    def __getitem__(self, item):
-        """Slicing support."""
-        if isinstance(item, slice):
-            # Construct an array of indices.
-            item = np.arange(*(item.indices(item.stop)))
-        return self.sample(item)
+    # def add(self, sndobj, offset=0):
+    #     '''Add a new sndobj to self.'''
+    #     self.sounds[sndobj] = sndobj
+    # 
+    # def stat(self):
+    #     '''Return a tuple containing the state of each sound object.'''
+    #     return tuple(self.sounds.values())
