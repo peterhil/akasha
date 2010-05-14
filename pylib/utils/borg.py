6b9a6181c255af56c111fff167afdcbb30e19b61
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-05-14 03:57:43 +0300

    (Re)moved those crap files into /misc.

--- pylib/utils/borg.py
+++
@@ -1,7 +0,0 @@
-class Borg:
-    """Borg pattern, see http://code.activestate.com/recipes/66531/
-    All objects share the same state - you will be assimilated!
-    """
-    __shared_state = {}
-    def __init__(self):
-        self.__dict__ = self.__shared_state
