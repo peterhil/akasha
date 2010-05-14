f7389e6305dee13dd3533a2e400d91ad5dda5b2e
Peter Hillerström <peter.hillerstrom@gmail.com>
2010-05-14 03:54:41 +0300

    Ventures into physical units and using traits.

---
+++ pylib/osc_units.py
@@ -0,0 +1,210 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+
+import numpy as np
+import types
+from cmath import rect, polar, phase, pi, exp
+from fractions import Fraction
+from numbers import Number
+# My modules
+from generators import PeriodicGenerator
+# from timing import Sampler
+
+import quantities as pq
+
+### Settings
+
+pq.markup.config.use_unicode = True  # Use unicode units representation
+# np.set_printoptions(precision=4, suppress=True)
+
+### Units
+Qt = pq.Quantity
+Hz = pq.Hz
+
+
+
+def to_phasor(x):
+    return (abs(x), (phase(x) / (2 * pi) * 360))
+
+
+class Sampler(object):
+    
+    _rate = Qt(44100.0, Hz)
+    
+    @property
+    def rate(self):
+        return self._rate
+    
+    def _set_frame_time(self):
+        pq.frame = pq.UnitTime('frame', Sampler.rate**-1, symbol='frame')
+    
+    @rate.setter
+    def rate(self, value):
+        self._rate = value
+        self._set_frame_time()
+    
+
+
+class Osc(object, PeriodicGenerator):
+    """Oscillator class"""
+
+    # Settings
+    prevent_aliasing = True
+    negative_frequencies = False
+
+    # Root cache is a dictionary
+    roots = dict()
+
+    # This could be __new__ if Osc would extend Fraction or be immutable
+    def __init__(self, *args):
+        # Set ratio and limit between 0/1 and 1/1
+        self._ratio = Osc.limit_ratio(Fraction(*args))
+
+        if not Osc.roots.has_key(self.period):
+            # Osc.roots[self.period] = self.gen_roots(self.func_root)   # 0.927 s
+            # Osc.roots[self.period] = self.gen_roots(self.nth_root)    # 0.731 s
+            # Osc.roots[self.period] = self.table_roots() # 0.057 s
+            Osc.roots[self.period] = self.np_exp() # 0.042 s
+        # self.roots = Osc.roots[self.period][self._root_order()]
+
+    @classmethod
+    def freq(cls, freq):
+        """Oscillator can be initialized using a frequency. Can be a float."""
+        ratio = Fraction.from_float(float(freq)/Sampler.rate).limit_denominator(Sampler.rate)
+        return cls(ratio)
+
+    @staticmethod
+    def limit_ratio(f):
+        if Osc.prevent_aliasing and abs(f) > Fraction(1,2):
+            return Fraction(0, 1)
+        if Osc.prevent_aliasing and not Osc.negative_frequencies and f < 0:
+            return Fraction(0, 1)
+        # wrap roots: 9/8 == 1/8 in Osc! This also helps with numeric accuracy.
+        n = f.numerator % f.denominator
+        return Fraction(n, f.denominator)
+
+    @property
+    def ratio(self):
+        return self._ratio
+    
+    @ratio.setter
+    def ratio(self, value):
+        self._ratio = value
+
+    @property
+    def period(self):
+        return self.ratio.denominator
+
+    @property
+    def order(self):
+        return self.ratio.numerator
+
+    @property
+    def frequency(self):
+        return float(self.ratio) * Sampler.rate
+
+    ### Generating functions ###
+
+    def normalize_fraction(self, n):
+        n %= self.period
+        return Fraction(n, self.period)
+
+    def nth_root(self, n):
+        w = 2 * pi
+        return rect(1, w * self.normalize_fraction(n))
+
+    def func_root(self, n):
+        wi = 2 * pi * 1j
+        return exp(wi * self.normalize_fraction(n))
+
+    def gen_roots(self, func):
+        return np.array(
+            map( func, range(0, self.period) ),
+            dtype=complex
+        )
+
+    def table_roots(self):
+        if not Osc.roots.has_key(Sampler.rate):
+            Osc.roots[Sampler.rate] = self.gen_table_roots()
+        return Osc.roots[Sampler.rate][0:self.period] ** (Sampler.rate / float(self.period))
+
+    def gen_table_roots(self):
+        w = 2 * pi
+        nth_root = lambda n: rect(1, w * Fraction(n, Sampler.rate))
+        return np.array(map(nth_root, range(0, Sampler.rate)))
+
+    def np_exp(self):
+        """Fastest generating method so far. Uses numpy.exp with linspace for angles.
+        Could be made still faster by using conjugate for half of the samples."""
+        return np.exp(np.linspace(0, 2 * pi, self.period, endpoint=False) * 1j)
+
+    ### Sampling ###
+
+    def _root_order(self):
+        return np.arange(self.period) * self.order % self.period
+
+    @property
+    def samples(self):
+        if self.order == 1:
+            return Osc.roots[self.period]
+        else:
+            return Osc.roots[self.period][self._root_order()]
+
+    ### Representation ###
+    
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.ratio == other.ratio
+        else:
+            return other == self.ratio
+    
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __repr__(self):
+        return "Osc(%s, %s)" % (self.order, self.period)
+
+    def __str__(self):
+        return "<Osc: %s Hz>" % self.frequency
+
+    def to_phasors(self):
+        return np.array(map(to_phasor, self.samples))
+
+    ### Arithmetic ###
+    # 
+    # See fractions.Fraction._operator_fallbacks() for automagic
+    # generation of forward and backward operator functions
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return Osc(self.ratio + other.ratio)
+        elif isinstance(other, float):
+            return Osc.freq(self.frequency + other)
+        elif isinstance(other, Number):
+            return Osc(self.ratio + other)
+        elif isinstance(other, np.ndarray) and isinstance(other[0], np.number):
+            return np.array(map(Osc.freq, self.frequency + other), dtype=np.object)
+        else:
+            raise NotImplementedError("Self: %s, other: %s", (self, other))
+    __radd__ = __add__
+
+    def __mul__(self, other):
+        if isinstance(other, self.__class__):
+            return Osc(self.ratio * other.ratio)
+        elif isinstance(other, float):
+            return Osc.freq(self.frequency * other)
+        elif isinstance(other, Number):
+            return Osc(self.ratio * other)
+        elif isinstance(other, np.ndarray) and isinstance(other[0], np.number):
+            return np.array(map(Osc.freq, self.frequency * other), dtype=np.object)
+        else:
+            raise NotImplementedError("Self: %s, other: %s", (self, other))
+    __rmul__ = __mul__
+
+
+if __name__ == '__main__':
+    o = Osc(Fraction(1, 8))
+    print o.roots
+    print o.to_phasors()
+    print Osc.roots.keys()
---
+++ pylib/units.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from cmath import rect, polar, phase, pi, exp
+from fractions import Fraction
+
+import quantities as pq
+
+# My modules
+
+
+# pq.markup.config.use_unicode = True  # Use unicode units representation
+# np.set_printoptions(precision=4, suppress=True)
+
+
+### Units
+Qt = pq.Quantity
+Hz = pq.Hz
+
