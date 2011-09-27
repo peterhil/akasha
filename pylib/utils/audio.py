54a20126f588c96cb3a220e983b4a90399e86154
Peter Hillerström <peter.hillerstrom@gmail.com>
2011-09-28 00:03:18 +0300

    Implemented support for multichannel audio formats.

--- pylib/utils/audio.py
+++ pylib/utils/audio.py
@@ -7,54 +7,88 @@ import os
 import numpy as np
 from scipy.signal import hilbert
 from scikits import audiolab
-from scikits.audiolab import Format, Sndfile
+from scikits.audiolab import Format, Sndfile, available_file_formats, available_encodings
 
 from timing import Sampler
 
+defaults = {
+    'type': 'aiff',
+    'encoding': 'pcm16',
+    'endianness': 'file'
+    }
+
+default_format = Format(**defaults)
+
+def get_format(*args, **kwargs):
+    """Get a Format object from a string, or parameters accepted by Format(type=wav, encoding=pcm16, endianness=file)"""
+    if (args and isinstance(args[0], Format)):
+        res = args[0]
+    else:
+        params = defaults.copy()
+        for i in xrange(len(args)):
+            params[['type', 'encoding', 'endianness'][i]] = args[i]
+        params.update(kwargs)
+        res = Format(**params)
+    return res
+
 # Audiolab read, write and play
 
-available_formats = set(map(lambda s: string.replace(s, 'write', ''), audiolab.__all__)) & set(audiolab.available_file_formats())
-
 def play(sndobj, axis='imag', fs=Sampler.rate, dur=5.0, start=0, time=False):
-    time = time or slice(int(round(0 + start)), int(round(dur * Sampler.rate + start)))
+    time = _slice_from(dur, start, time)
     audiolab.play(getattr(sndobj[time], axis), fs)
 
-def write(sndobj, filename='test_sound', axis='imag', format='aiff', enc='pcm16',
+def write(sndobj, filename='test_sound', axis='imag', fmt=Format(**defaults),
           fs=Sampler.rate, dur=5.0, start=0, time=False,
-          sdir='../../Sounds/2010_Python_Resonance/', *args, **kwargs):
+          sdir='../../Sounds/2010_Python_Resonance/'):
 
-    # Check that format is available
-    if format not in available_formats:
-        raise ValueError("File format '%s' not available. Try one of: %s" % (format, list(available_formats)))
+    fmt = get_format(fmt)
+    filename = sdir + filename +'_'+ axis +'.'+ fmt.file_format
 
-    # Use time (=slice obj) OR the provided attributes dur and start
-    time = time or slice(int(round(0 + start)), int(round(dur * Sampler.rate + start)))
+    time = _slice_from(dur, start, time)
+    data = getattr(sndobj[time], axis)
 
-    # Get and call appropriate writer function
-    func = getattr(audiolab, format + 'write')
-    return func(getattr(sndobj[time], axis), sdir + filename +'_'+ axis +'.'+ format, fs, enc)
+    if np.ndim(data) <= 1:
+        n_channels = 1
+    elif np.ndim(data) == 2:
+        n_channels = data.shape[1]
+    else:
+        RuntimeError("Only rank 0, 1, and 2 arrays supported as audio data")
 
-def read(filename, fs=Sampler.rate, dur=5.0, start=0, time=False, complex=True,
+    hdl = Sndfile(filename, 'w', fmt, n_channels, int(fs))
+    try:
+        hdl.write_frames(data)
+    finally:
+        hdl.close()
+
+
+def read(filename, dur=5.0, start=0, time=False, fs=Sampler.rate, complex=True,
          sdir='../../Sounds/_Music samples/', *args, **kwargs):
-    """Reading function. Useful for doing some analysis. Audiolab has the same read as write functions!"""
+    """Reading function. Useful for doing some analysis."""
+
+    # Process:
+    # Get audiofile in
+    # Process
+    # Return complex audio data
+
+    # Operation:
+    # - examine file extension to determine type
+
+    # TODO:
+    # - Factor out the real->complex conversion out.
 
     if filename[0] != '/':    # Relative path
         filename = sdir + filename
 
     format = os.path.splitext(filename)[1][1:]
-
-    # Check that format is available
-    if format not in available_formats:
-        raise ValueError("File format '%s' not available. Try one of: %s" % (format, list(available_formats)))
-
-    # Use time (=slice obj) OR the provided attributes dur and start
-    time = time or slice(int(round(0 + start)), int(round(dur * Sampler.rate + start)))
+    check_format(format)
+    time = _slice_from(dur, start, time)
 
     # Get and call appropriate reader function
     func = getattr(audiolab, format + 'read')
 
     # Read data
-    (data, fs, enc) = func(filename, last=time.stop, first=start)   # returns (data, fs, enc)
+    (data, fs, enc) = func(filename, last=time.stop, first=time.start)   # returns (data, fs, enc)
+
     # TODO if fs or enc differs from default do some conversion?
     if data.ndim > 1:
         data = data.transpose()[-1]  # Make mono, take left [0] or right [-1] channel.
@@ -64,3 +98,21 @@ def read(filename, fs=Sampler.rate, dur=5.0, start=0, time=False, complex=True,
         data = hilbert(data)
 
     return data
+
+def check_format(format):
+    """Checks that a requested format is available (in libsndfile)."""
+    available = available_file_formats()
+    if format not in available:
+        raise ValueError("File format '%s' not available. Try one of: %s" % (format, available))
+
+def check_encoding(format):
+    pass
+
+def _slice_from(dur, start, time=False):
+    """Use a time slice argument or the provided attributes 'dur' and 'start' to
+    construct a time slice object."""
+    time = time or slice(int(round(0 + start)), int(round(dur * Sampler.rate + start)))
+    if not isinstance(time, slice):
+        raise TypeError("Expected a %s for 'time' argument, got %s." % (slice, type(time)))
+    return time
+
