db27da83f332aa3f26f73163ea8e2c85077da5b6
Peter Hillerström <peter.hillerstrom@gmail.com>
2012-11-03 18:30:58 +0200

    Made tests more readable.

--- akasha/test/audio/frequency_test.py
+++ akasha/test/audio/frequency_test.py
@@ -23,12 +23,6 @@ from numpy.testing.utils import assert_array_almost_equal_nulp as assert_nulp_di
 from numpy.testing.utils import nulp_diff
 
 
-# Just noticeable difference allowance for Frequency rounding
-# See http://en.wikipedia.org/wiki/Cent_(music)#Human_perception
-# This should probably be much smaller than the suggested 3-6 cents...
-JND_CENTS_EPSILON = 1.0e-2
-
-
 class TestFrequency(object):
     """Test frequencies"""
 
@@ -45,47 +39,56 @@ class TestFrequency(object):
 
     def test_float_frequency(self):
         """It should return close frequency from float."""
+
+        # Just noticeable difference allowance for Frequency rounding
+        # See http://en.wikipedia.org/wiki/Cent_(music)#Human_perception
+        # This should probably be much smaller than the suggested 3-6 cents...
+        JND_CENTS_EPSILON = 1.0e-2
+
         for f in [20.899, 30.0001, 220.0001, 440.899, 2201.34, 8001.378, 12003.989, 20000.1]:
             o = Osc(f)
             assert cents_diff(o.frequency, f) <= JND_CENTS_EPSILON
 
 
 class TestFrequencyAliasing(object):
-    """Test (preventing the) aliasing of frequencies: f < 0, f > sample rate"""
+    """Test (preventing the) aliasing of frequencies out of range 0 to sample rate."""
+    
     silence = Frequency(0)
+    negative = Fraction(-1, 7)
+    over_nyquist = Fraction(9, 14)
+    over_one = Fraction(9, 7)
 
-    def testFrequenciesOverNyquistRate(self):
-        """It should (not) produce silence if ratio > 1/2"""
-        sampler.prevent_aliasing = True
-        assert Frequency.from_ratio(9, 14) == self.silence
+    @staticmethod
+    def freq(ratio):
+        return Frequency.from_ratio(ratio)
 
-        sampler.prevent_aliasing = False
-        assert Frequency.from_ratio(9, 14).ratio == Fraction(9, 14)
-
-    def testFrequenciesOverSamplingRate(self):
-        sampler.prevent_aliasing = True
-        assert Frequency.from_ratio(9, 7) == self.silence
-
-        sampler.prevent_aliasing = False
-        assert Frequency.from_ratio(9, 7).ratio == Fraction(2, 7)
-
-    def testNegativeFrequencies(self):
-        """It should handle negative frequencies according to preferences."""
+    def testPreventAliasing(self):
+        """It should prevent aliasing when given a ratio out of range 0 to 1/2."""
         sampler.prevent_aliasing = True
         sampler.negative_frequencies = False
-        assert Frequency.from_ratio(-1, 7) == self.silence
+
+        assert self.freq(self.negative) == self.silence
+        assert self.freq(self.over_nyquist) == self.silence
+        assert self.freq(self.over_one) == self.silence
+
+    def testFrequenciesOverNyquistRate(self):
+        """It should not produce silence if ratio > 1/2"""
+        sampler.prevent_aliasing = False
+        assert self.freq(self.over_nyquist).ratio == self.over_nyquist
+
+    def testFrequenciesOverSamplingRate(self):
+        """It should wrap when ratio is over one."""
+        sampler.prevent_aliasing = False
+        assert self.freq(self.over_one).ratio == self.over_one % 1
+
+    def testNegativeFrequencies(self):
+        """It should wrap negative frequencies."""
+        sampler.negative_frequencies = True
 
         sampler.prevent_aliasing = False
-        sampler.negative_frequencies = True
-        assert Frequency.from_ratio(-1, 7) == Frequency.from_ratio(6, 7)
+        assert self.freq(self.negative).ratio == self.negative % 1
 
         sampler.prevent_aliasing = True
-        sampler.negative_frequencies = True
-        ratio = Fraction(-1, 7)
-        f = Frequency.from_ratio(ratio)
-        assert f.frequency == float(ratio * sampler.rate) == -6300.0
-
-        # Note! f.ratio is modulo sampler.rate for negative frequencies
-        assert f.ratio == Fraction(6, 7)
+        assert self.freq(self.negative).ratio == self.negative % 1
 
 
