d73e4465ce9e8476dc975f0e029fa89b00930adb
Peter Hillerström <peter.hillerstrom@gmail.com>
2012-11-04 15:11:59 +0200

    Python 2.7 compatibility: Added 'UTF-8' in the check for locale. Increased edgeitems.

--- akasha/settings/__init__.py
+++ akasha/settings/__init__.py
@@ -9,14 +9,14 @@ import sys
 
 
 def setup():
-    np.set_printoptions(precision=16, threshold=1000, edgeitems=10, linewidth=78, suppress=True)
+    np.set_printoptions(precision=16, threshold=1000, edgeitems=40, linewidth=78, suppress=True)
 
     # Set the user's default locale, see http:// docs.python.org/library/locale.html
     # Also be sure to have LC_ALL='fi_FI.UTF-8' and CHARSET='UTF-8' set in the environment
     # to have sys.stdin.encoding = UTF-8
     locale.setlocale(locale.LC_ALL, 'fi_FI.UTF-8')
 
-    assert locale.getlocale()[1] == 'UTF8', \
+    assert locale.getlocale()[1] in ('UTF8', 'UTF-8'), \
         "Unicode not enabled! Current locale is: %s.%s" % locale.getlocale()
 
     if isinstance(sys.stdin, file):
