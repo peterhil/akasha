a753ca9eb82b419d1005f113fb064f8bed19b715
Peter Hillerström <peter.hillerstrom@gmail.com>
2012-12-25 19:11:48 +0200

    Use map_array instead of map.

--- akasha/types/__init__.py
+++ akasha/types/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Types module
+"""
 
 import numpy as np
+
+from akasha.utils.math import map_array
+
 
 colour_values = np.float32
 colour_result = np.float32
@@ -10,5 +16,8 @@ signed = (int, float, np.signedinteger, np.floating)
 
 
 def assert_type(types, *args):
-    assert np.all(map(lambda p: isinstance(p, types), args)), \
-        "All arguments must be instances of %s, got:\n%s" % (types, map(type, args))
+    """
+    Assert that all the arguments are instances of the specified types.
+    """
+    assert np.all(map_array(lambda p: isinstance(p, types), args)), \
+        "All arguments must be instances of %s, got:\n%s" % (types, map_array(type, args))
