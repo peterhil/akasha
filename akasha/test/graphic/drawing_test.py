0384a85e5b7072a7cbcb9c4a9e31ac6403bf09f0
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-01-06 12:23:37 +0200

    Removed the incomplete graph test.

--- akasha/test/graphic/drawing_test.py
+++ akasha/test/graphic/drawing_test.py
@@ -16,48 +16,6 @@ import pytest
 from akasha.graphic.drawing import *
 
 
-class TestGraph(object):
-    """Tests for graph()"""
-
-    @pytest.skip("Incomplete")
-    def test_graph_coord_bounds(self):
-        """Test coordinate boundaries"""
-
-        ### Make complex signal containing grid points
-
-        x_axis = y_axis = np.linspace(-1, 1, 40)
-
-        # In [213]: x_axis
-        # Out[213]: array([-1.    , -0.3333,  0.3333,  1.    ])
-        #
-        # In [214]: y_axis
-        # Out[214]: array([-1.    , -0.3333,  0.3333,  1.    ])
-        #
-
-        grid = np.array(np.meshgrid(x_axis, y_axis))
-
-        # In [281]: grid
-        # Out[281]:
-        # array([[[-1.    , -0.3333,  0.3333,  1.    ],
-        #         [-1.    , -0.3333,  0.3333,  1.    ],
-        #         [-1.    , -0.3333,  0.3333,  1.    ],
-        #         [-1.    , -0.3333,  0.3333,  1.    ]],
-        #
-        #        [[-1.    , -1.    , -1.    , -1.    ],
-        #         [-0.3333, -0.3333, -0.3333, -0.3333],
-        #         [ 0.3333,  0.3333,  0.3333,  0.3333],
-        #         [ 1.    ,  1.    ,  1.    ,  1.    ]]])
-
-        samples = grid.copy().transpose().view(np.complex).flatten()
-
-        # In [286]: samples
-        # Out[286]:
-        # array([-1.-1.j    , -1.-0.3333j, -1.+0.3333j, ...,  1.-0.3333j,
-        #         1.+0.3333j,  1.+1.j    ])
-
-        # graph(samples)
-
-
 class TestDrawing(object):
     """
     Unit tests for drawing module.
