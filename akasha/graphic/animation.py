c1928ed9a1d6440d32c389dc789bfb458834d7d5
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-01-08 15:11:12 +0200

    Use lambda instead of xoltar.curry_function.

--- akasha/graphic/animation.py
+++ akasha/graphic/animation.py
@@ -253,8 +253,16 @@ def anim(snd, size=800, name="Resonance", antialias=True, lines=False, colours=T
     ch = pg.mixer.find_channel()
     it = iter(snd)
 
-    paint_fn = fx.curry_function(show_slice, screen, size=size,
-                        antialias=antialias, lines=lines, colours=colours)
+    paint_fn = lambda snd: show_slice(
+        screen,
+        snd,
+        size=size,
+        antialias=antialias,
+        lines=lines,
+        colours=colours
+    )
+    # paint_fn = fx.curry_function(show_slice, screen, size=size,
+    #                     antialias=antialias, lines=lines, colours=colours)
     #paint_fn = fx.curry_function(show_transfer, screen, size=size, standard='PAL', axis='imag')
 
     clock = pg.time.Clock()
