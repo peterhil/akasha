ca20bd885625a26f4dfcf2fed75115d8de68ddec
Peter Hillerström <peter.hillerstrom@soprano.fi>
2013-10-22 20:13:14 +0300

    Move import from akasha/__init__.py into akasha/lab.py.

---
+++ akasha/lab.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Akasha audio program suite interactive lab.
+"""
+
+from __future__ import absolute_import
+
+import numpy as np
+
+from cmath import rect, pi, exp, phase
+from funckit import xoltar as fx
+from scipy.signal import hilbert
+
+from akasha.audio.curves import *
+from akasha.audio.dtmf import DTMF
+from akasha.audio.envelope import Attack, Exponential, Gamma
+from akasha.audio.frequency import Frequency
+from akasha.audio.harmonics import Overtones
+from akasha.audio.noise import *
+from akasha.audio.oscillator import *
+from akasha.audio.sound import Sound, Group, Pcm
+
+from akasha.control.io.audio import play, write, read
+from akasha.control.io.keyboard import *
+
+from akasha.effects.tape import *
+from akasha.effects.filters import *
+
+from akasha.graphic.animation import *
+from akasha.graphic.drawing import *
+from akasha.graphic.geometry import *
+from akasha.graphic.primitive.line import *
+from akasha.graphic.primitive.spline import *
+
+from akasha.net.wiki import *
+
+from akasha.timing import sampler
+from akasha.tunings import WickiLayout
+
+from akasha.utils.math import *
