9a4b09ef32b582b438635657a9e0d882c48a672c
Peter Hillerström <peter.hillerstrom@gmail.com>
2015-02-17 22:11:55 +0200

    Add unit_step and unit_impulse on akasha.dsp.

--- akasha/dsp/__init__.py
+++ akasha/dsp/__init__.py
@@ -3,3 +3,21 @@
 """
 DSP module.
 """
+
+import numpy as np
+
+
+@np.vectorize
+def unit_step(x):
+    """
+    Discrete Heaviside unit step function.
+    """
+    return 0 if x < 0 else 1
+
+
+@np.vectorize
+def unit_impulse(x):
+    """
+    Kronecker delta function.
+    """
+    return 1 if x == 0 else 0
