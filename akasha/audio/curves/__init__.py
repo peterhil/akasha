7698ea2b10da2f8294e0710740a4e7b8750d4c75
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-08-21 01:58:51 +0300

    Move chirp_zeta() into own module under dsp

--- akasha/audio/curves/__init__.py
+++ akasha/audio/curves/__init__.py
@@ -6,29 +6,8 @@ Curves for oscillators
 
 from __future__ import division
 
-import numpy as np
-
 from akasha.audio.curves.circle import Circle
 from akasha.audio.curves.curve import Curve
 from akasha.audio.curves.ellipse import Ellipse
 from akasha.audio.curves.square import Square
 from akasha.audio.curves.super import Super
-
-from akasha.timing import sampler
-from akasha.utils.math import normalize
-
-
-def chirp_zeta(z1=-0.5 - 100j, z2=0.5 + 100j, dur=10):
-    """
-    Chirp sound made by sampling a line z (z1 -> z2) from the complex plane,
-    and using the function (k ** -z, k = 0..n) used for summation in the Riemann Zeta function.
-
-    Other interesting values to try:
-    chirp_zeta(-10.5-1000j, 1.5-10000j)
-
-    Reference: http://en.wikipedia.org/wiki/Riemann_zeta_function
-    """
-    n = int(round(dur * sampler.rate))
-    z = np.linspace(z1, z2, n)
-    k = np.arange(n)
-    return normalize(k ** -z)
