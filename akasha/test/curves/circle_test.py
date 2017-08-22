05b354c6cb47e68946181871e512f07e1264ab67
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-08-22 16:44:38 +0300

    Split curves tests into separate files

---
+++ akasha/test/curves/circle_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# C0111: Missing docstring
+# R0201: Method could be a function
+# E1101: Module 'x' has no 'y' member
+#
+# pylint: disable=C0111,R0201,E1101
+
+import numpy as np
+
+from numpy.testing.utils import assert_array_almost_equal_nulp as assert_nulp_diff
+
+from akasha.curves import Curve, Circle
+from akasha.utils.math import pi2
+from akasha.utils.patterns import Singleton
+
+
+class TestCircle(object):
+    """
+    Unit tests for Circle
+    """
+
+    def test_super(self):
+        assert issubclass(Circle, Curve)
+        assert issubclass(Circle, Singleton)
+
+    def test_at(self):
+        c = Circle()
+        pts = np.linspace(0, 1.0, 7, endpoint=False)
+        assert_nulp_diff(c.at(pts), np.exp(pi2 * 1j * pts), 1)
+
+    def test_roots_of_unity(self):
+        points = np.linspace(0, 1, 5, endpoint=False)
+        expected = Circle.at(points)
+        assert np.all(Circle.roots_of_unity(5) == expected)
+
+    def test_at_complex(self):
+        c = Circle()
+        pts = np.linspace(0, 1 + 1j, 7, endpoint=False)
+        assert_nulp_diff(c.at(pts), np.exp(pi2 * 1j * pts), 1)
+
+    def test_at_isperiodic(self):
+        assert_nulp_diff(Circle.at(np.arange(-1, 3, 1)), 1 + 0j, 3)
+
+    def test_repr(self):
+        o = Circle()
+        assert o == eval(repr(o))
