592032785df4ec4afae601960db4cfa3738203e0
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-09-01 18:52:18 +0300

    Do not call super on Exponential.__init__()

--- akasha/audio/envelope/exponential.py
+++ akasha/audio/envelope/exponential.py
@@ -22,7 +22,7 @@ class Exponential(Generator):
     """
 
     def __init__(self, rate=0.0, amp=1.0):
-        super(self.__class__, self).__init__()
+        # super(self.__class__, self).__init__()
 
         if isinstance(rate, tuple):
             self.rate, self.amp = np.array(rate).astype(np.float64)
