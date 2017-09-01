cd2ddb721b275c0d91c65aa00d190edeb26d91f4
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-09-02 01:33:37 +0300

    Remove old Attack object

--- akasha/audio/envelope/__init__.py
+++ akasha/audio/envelope/__init__.py
@@ -12,34 +12,10 @@ Envelopes
 import numpy as np
 import scipy.special as sc
 
-from akasha.audio.generators import Generator
-from akasha.audio.envelope.attack import Attack
-from akasha.audio.envelope.exponential import Exponential
 from akasha.audio.envelope.adsr import Adsr
+from akasha.audio.envelope.exponential import Exponential
+from akasha.audio.generators import Generator
 from akasha.timing import sampler
-
-
-class Attack(Exponential):
-    """
-    Exponential attack (reversed decay/growth) envelope
-    """
-
-    def __init__(self, rate=0.0, amp=1.0, *args, **kwargs):
-        super(self.__class__, self).__init__(rate, amp, *args, **kwargs)
-
-    def sample(self, iterable, threshold=1.0e-6):
-        """
-        Sample the attack envelope.
-        """
-        attack = super(self.__class__, self).sample(iterable)
-        attack = attack[attack > threshold][::-1]  # Filter silence and reverse
-
-        frames = np.zeros(len(iterable))
-        frames.fill(attack[-1])  # Sustain level
-        frames[:len(attack)] = attack
-
-        del(attack)
-        return frames
 
 
 class Gamma(Generator):
