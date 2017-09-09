73f79bd0cd62245608330d476fb54573d2336b65
Peter Hillerström <peter.hillerstrom@gmail.com>
2017-09-09 20:58:28 +0300

    Lint the graphics output module

--- akasha/graphic/output.py
+++ akasha/graphic/output.py
@@ -9,8 +9,8 @@ Graphics output module
 """
 
 import os
-import pylab as lab
 import tempfile
+import pylab as lab
 
 from PIL import Image
 
@@ -33,15 +33,20 @@ def show(img, plot=False, osx_open=False):
         lab.show(False)
     elif osx_open:
         try:
-            tmp = tempfile.NamedTemporaryFile(dir='/var/tmp', prefix='akasha_', suffix='.png', delete=False)
-            logger.debug("Tempfile: %s" % tmp.name)
+            tmp = tempfile.NamedTemporaryFile(
+                dir='/var/tmp',
+                prefix='akasha_',
+                suffix='.png',
+                delete=False
+            )
+            logger.debug("Tempfile: %s", tmp.name)
             image = Image.fromarray(img)
             image.save(tmp.name, 'png')
             os.system("open " + tmp.name)
         except IOError, err:
-            logger.error("Failed to open a temporary file and save the image: %s" % err)
+            logger.error("Failed to open a temporary file and save the image: %s", err)
         except OSError, err:
-            logger.error("Failed to open the image with a default app: %s" % err)
+            logger.error("Failed to open the image with a default app: %s", err)
         finally:
             tmp.close()
     else:
@@ -50,26 +55,25 @@ def show(img, plot=False, osx_open=False):
 
 
 def imsave(img, filename):
+    """
+    Save the image into file.
+    """
     try:
         img = img.transpose((1, 0, 2))
         image = Image.fromarray(img)
         image.save(filename, 'png')
     except IOError, err:
-        logger.error("Failed to save image into {}".format(filename))
-    except Exception, err:
-        logger.error("Error when saving image: {}".format(err))
+        logger.error("Failed to save image into file: '%s'\n\nError was: %s", filename, err)
+    except OSError, err:
+        logger.error("OS error when saving image: %s", err)
+    finally:
+        image.close()
 
 
-def graph(signal, size=1000, plot=False, axis=True,
-          antialias=True, lines=False, colours=True, img=None, osx_open=False):
+def graph(signal, plot=False, osx_open=False, **kwargs):
     """
     Make an image from the sound signal and show it.
+    Accepts the same keyword arguments as draw().
     """
-    img = draw(
-        signal,
-        size=size,
-        antialias=antialias, lines=lines, colours=colours,
-        axis=axis,
-        img=img
-    )
+    img = draw(signal, **kwargs)
     show(img, plot, osx_open)
