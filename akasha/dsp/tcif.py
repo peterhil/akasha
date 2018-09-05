c248dac0388558c0d5cd0726ead920db0bb5bda0
Peter Hillerström <peter.hillerstrom@gmail.com>
2018-09-05 20:54:11 +0300

    First try at implementing tcif algorithm by Kodera et al. Probably does not work yet correctly.

---
+++ akasha/dsp/tcif.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# E1101: Module 'x' has no 'y' member
+#
+# pylint: disable=E1101
+
+"""
+Time corrected instantaneous frequency (TCIF) algorithms module.
+"""
+
+import numpy as np
+import scipy as sc
+
+from akasha.dsp.fft import windowed_frames, stft
+from akasha.math.functions import pad, pi2, power_limit
+from akasha.timing import sampler
+
+
+__all__ = ['tcif']
+
+
+def tcif(signal, window_size=1901, hop=None, method='kodera'):
+    """
+    TCIF algorithm using the finite difference approximations by Kodera et al.
+    """
+    if hop is None: hop = window_size // 2
+    # Find the next power of two for n_fft
+    n_fft = int(power_limit(window_size, base=2, rounding=np.ceil))
+
+    # Step 1 and 2, build the matrices and do three stfts
+
+    delayed_signal = pad(signal[1:-1], index=0, count=1, value=0)  # Or use np.roll?
+    # TODO Try other windows than hamming
+    args = dict(n_fft=n_fft, frame_size=window_size, hop=hop, window=sc.signal.hamming, roll=False, normalize=True)
+    s = stft(signal, **args)
+    s_delayed = stft(delayed_signal, **args)
+    s_freqdel = np.roll(s, 1, axis=0)  # TODO Check that frquencies are rolled up by one!
+
+    # Step 3, compute channelized instantaneous frequency and local group delay
+
+    if method == 'kodera':
+        cif = (-sampler.rate / pi2) * np.mod(np.abs(s_delayed) - np.abs(s), pi2)
+        lgd = (n_fft / (pi2 * sampler.rate)) * np.mod(np.abs(s_freqdel) - np.abs(s), pi2)
+    elif method == 'nelson':
+        raise NotImplementedError('Nelson method is not implemented yet')
+    else:
+        raise NotImplementedError('Given method is not implemented')
+
+    return (cif, lgd)  # TODO Implement plotting (in a separate function?)
