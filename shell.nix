969151035fecc3dcbdbc1ce7c3fc8be212f6119a
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-25 01:26:57 +0300

    nix: Try to get scikit libraries to work without success – frustration

--- shell.nix
+++ shell.nix
@@ -1,19 +1,32 @@
-{
-  pkgs ?
-  import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz") {}
-}:
+# shell.nix
+
+with import (
+    fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz"
+) {};
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    SDL2
-    SDL2_image
-    SDL2_mixer
-    SDL2_ttf
-    freetype
-    libogg
-    libpng
-    libsamplerate
-    libsndfile
-    portaudio
-  ];
+    nativeBuildInputs = with pkgs; [
+        SDL2
+        SDL2_image
+        SDL2_mixer
+        SDL2_ttf
+        # blas
+        freetype
+        # gfortran
+        # lapack
+        libogg
+        libpng
+        libsamplerate
+        libsndfile
+        portaudio
+    ];
+
+    shellHook = ''
+        # export LD_LIBRARY_PATH="${pkgs.libsamplerate}/lib:${pkgs.libsndfile}/lib:${pkgs.portaudio}/lib:$LD_LIBRARY_PATH"
+
+        # export DYLD_FALLBACK_LIBRARY_PATH="${pkgs.libsamplerate}/lib:${pkgs.libsndfile}/lib:${pkgs.portaudio}/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+
+        # Change to 10.04 when Nix switches to newer SDK:
+        # export MACOSX_DEPLOYMENT_TARGET="10.12"
+    '';
 }
