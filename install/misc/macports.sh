f4a237cdf611fbf21ffaad761bc3f540b94ad044
Peter Hillerström <peter.hillerstrom@gmail.com>
2021-06-25 12:04:57 +0300

    Revert "Remove scikits.audiolab from dependencies as it is last updated six years ago and does not support Python 3"
    This reverts commit a33c48f7dc66eb163cd3ea288435f6edbba95237.

--- install/misc/macports.sh
+++ install/misc/macports.sh
@@ -4,7 +4,7 @@
 port install freetype
 # Pygame depends
 port install portmidi libsdl-framework libsdl_mixer-framework libsdl_ttf-framework libsdl_image-framework
-# python-soundfile depends
+# scikits.audiolab depends
 port install libsndfile
 # scikits.samplerate depends
 port install libsamplerate
