c4268fcdb8d031709543222e7eacde4449590530
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-01-26 15:24:22 +0200

    Move file_extension and relative_path into io.path

--- akasha/io/__init__.py
+++ akasha/io/__init__.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-
-from akasha.settings import config
-
-
-def relative_path(path, base=config.basedir):
-    return os.path.abspath('/'.join([base, path]))
-
-def file_extension(filename):
-    return os.path.splitext(filename)[1][1:]
+from akasha.io.path import file_extension, relative_path
