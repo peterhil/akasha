d19b12113cfef8e86fd4157d8425c3dbfa1de557
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-01-26 17:24:35 +0200

    Move Watch into timing.watch

--- akasha/timing/__init__.py
+++ akasha/timing/__init__.py
@@ -15,9 +15,8 @@ import numpy as np
 
 from timeit import default_timer as timer
 
-from akasha.utils.array import is_empty
-from akasha.utils.log import logger
 from akasha.timing.sampler import sampler
+from akasha.timing.watch import Watch
 
 
 def time_slice(dur, start=0, time=False):
@@ -46,53 +45,3 @@ class Timed(object):
 
     def __float__(self):
         return float(self.elapsed)
-
-
-class Watch(object):
-    def __init__(self, maxstops=5):
-        self.paused = 0
-        self.maxstops = maxstops
-        self.timings = []
-        self.reset()
-
-    def reset(self):
-        self.epoch = timer()
-        if self.paused:
-            self.paused = self.epoch
-        self.lasttime = 0
-
-    def time(self):
-        if not self.paused:
-            return timer() - self.epoch
-        else:
-            return self.paused - self.epoch
-
-    def last(self):
-        return self.time() - self.lasttime
-
-    def next(self):
-        if not self.paused:
-            self.lasttime = self.time()
-            self.timings.append(self.lasttime)
-            self.timings = self.timings[-self.maxstops:]
-        return self.lasttime
-
-    def pause(self):
-        if not self.paused:
-            self.paused = timer()
-        else:
-            if self.paused > 0:
-                self.epoch += timer() - self.paused
-            self.paused = 0
-
-    def get_fps(self, n=None):
-        if n is None:
-            ts = np.ediff1d(np.array(self.timings))
-        elif n > 0:
-            ts = np.ediff1d(np.array(self.timings[-n:]))
-
-        # print('timings:', ts)
-        ts = ts[ts >= 2e-3]  # Filter trash values after reset
-        if is_empty(ts): return 0
-
-        return np.median(1.0 / ts)
