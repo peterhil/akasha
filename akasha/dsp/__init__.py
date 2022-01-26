a7665a49b5a4408a6842430fe0aeb0976d7ff505
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-01-26 15:46:13 +0200

    Move step functions into dsp.functions

--- akasha/dsp/__init__.py
+++ akasha/dsp/__init__.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# E1101: Module 'x' has no 'y' member
-#
-# pylint: disable=E1101
 
 """
-DSP module.
+DSP module
 """
 
 # import librosa as rosa
-import numpy as np
 
+from akasha.dsp.functions import *
 from akasha.dsp.window import *
 from akasha.dsp.z_transform import *
-
-
-@np.vectorize
-def unit_step(x):
-    """
-    Discrete Heaviside unit step function.
-    """
-    return 0 if x < 0 else 1
-
-
-@np.vectorize
-def unit_impulse(x):
-    """
-    Kronecker delta function.
-    """
-    return 1 if x == 0 else 0
