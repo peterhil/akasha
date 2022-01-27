0270e8dd882948d1bed6d1b529ef8ff8616129a4
Peter Hillerström <peter.hillerstrom@gmail.com>
2022-01-27 04:00:34 +0200

    Use lazy logging and f-strings on settings

--- akasha/settings/numpy.py
+++ akasha/settings/numpy.py
@@ -15,16 +15,26 @@ def np_setup():
     """
     Setup numpy and locale settings.
     """
-    np.set_printoptions(precision=16, threshold=1000, edgeitems=40, linewidth=78, suppress=True)
+    np.set_printoptions(
+        precision=16,
+        threshold=1000,
+        edgeitems=40,
+        linewidth=78,
+        suppress=True,
+    )
 
-    # Set the user's default locale, see http:// docs.python.org/library/locale.html
-    # Also be sure to have LC_ALL='fi_FI.UTF-8' and CHARSET='UTF-8' set in the environment
-    # to have sys.stdin.encoding = UTF-8
+    # Set the user's default locale, see:
+    # http:// docs.python.org/library/locale.html
+    #
+    # Also be sure to have LC_ALL='fi_FI.UTF-8' and CHARSET='UTF-8' set in the
+    # environment to have sys.stdin.encoding = UTF-8
     locale.setlocale(locale.LC_ALL, 'fi_FI.UTF-8')
 
+    current_locale = '.'.join(locale.getlocale())
     assert locale.getlocale()[1] in ('UTF8', 'UTF-8'), \
-        "Unicode not enabled! Current locale is: %s.%s" % locale.getlocale()
+        'Unicode not enabled! Current locale is: ' + current_locale
 
     if six.PY2 and isinstance(sys.stdin, file):
         assert sys.stdin.encoding == 'UTF-8', \
-            "Unicode input not enabled! Current input encoding is: %s" % sys.stdin.encoding
+            'Unicode input not enabled! Current input encoding is: ' + \
+            sys.stdin.encoding
